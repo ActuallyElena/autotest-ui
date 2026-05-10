4709e50294336380d2a8a2982bfbff617bbe4605
ActuallyElena <elenamikhailova23091999@gmail.com>
2026-05-10 12:06:10 +0300

    Практикуемся в написании автотестов с использованием POM

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -1,8 +1,12 @@
 import pytest
 from playwright.sync_api import Page
 
-from fixtures.browsers import chromium_page, chromium_page_with_state
-from pages.courses_list_page import CourseListPage
+from pages.courses_list_page import CoursesListPage
+from pages.dashboard_page import DashboardPage
+import pytest
+from playwright.sync_api import Page
+
+from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
 from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
@@ -10,27 +14,26 @@ from pages.registration_page import RegistrationPage
 
 
 @pytest.fixture
-def login_page(chromium_page:Page) -> LoginPage:
+def login_page(chromium_page: Page) -> LoginPage:
     return LoginPage(page=chromium_page)
 
 
-
 @pytest.fixture
-def registration_page(chromium_page:Page) -> RegistrationPage:
+def registration_page(chromium_page: Page) -> RegistrationPage:
     return RegistrationPage(page=chromium_page)
 
 
-
 @pytest.fixture
-def dashboard_page(chromium_page:Page) -> DashboardPage:
+def dashboard_page(chromium_page: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page)
 
 
 @pytest.fixture
-def courses_list_page(chromium_page:Page) -> CourseListPage:
-    return CourseListPage(page=chromium_page)
+def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
+    return CoursesListPage(page=chromium_page_with_state)
 
 
+# Добавили эту фикстуру в данном шаге
 @pytest.fixture
-def create_course_page(chromium_page_with_state:Page) -> CreateCoursePage:
+def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
     return CreateCoursePage(page=chromium_page_with_state)
